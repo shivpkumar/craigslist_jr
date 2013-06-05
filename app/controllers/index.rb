bcdddd676471d4165c1c503e380c6003c0179528
Shiv Kumar <shivpkumar@gmail.com>
2013-06-04 22:13:43 -0700

    added update functionality

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,18 @@
-get '/' do
-  # Look in app/views/index.erb
-  erb :index
+get '/posts/:key/edit' do
+	@post = Post.find_by_secret_key(params[:key])
+	puts @post
+	erb :edit_post
+end
+
+post '/posts/:id' do
+	@post = Post.find(params[:id])
+	post_attrs = params[:post]
+	@post.update_attributes(post_attrs)
+	@post.save
+	redirect "posts/#{@post.id}"
+end
+
+get '/posts/:id' do
+	@post = Post.find(params[:id])
+	erb :display_post
 end
